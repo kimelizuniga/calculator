f800f94039537c02a668877166f964695c88fc72
Kim Zuniga <kim.eli.zuniga@gmail.com>
2021-10-21 17:08:24 -0400

    Create add function and number buttons

---
+++ index.js
@@ -0,0 +1,41 @@
+let deleteBtn = document.getElementById('deleteBtn')
+let plusBtn = document.getElementById('plusBtn')
+let previous_operand = document.getElementById('previous_operand')
+let current_operand = document.getElementById('current_operand')
+let numberString = document.getElementsByClassName('numberBtn')
+let previousNum;
+let currentNum = "";
+let number;
+let sum;
+
+function CombineStringNum(num)
+{
+    if (currentNum == 0)
+        currentNum = num;
+    else
+        currentNum += num;
+    
+    
+    current_operand.innerText = currentNum;
+}
+
+function ResetCurrentOperand()
+{
+    current_operand.innerText = "0";
+}
+
+function AddNum(num)
+{
+    currentNum += num;
+    currentNum = parseInt(currentNum);
+    if (previous_operand.innerText != "")
+    {
+        sum = parseInt(previous_operand.innerText) + currentNum;
+        previous_operand.innerText = sum;
+    }
+    else 
+        previous_operand.innerText = currentNum;
+    current_operand.innerText = "0";
+    currentNum = "0";
+
+}
