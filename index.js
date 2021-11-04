60e67cef49c2aea015daa21806bb3a4b7bff88b4
Deukhwa <deukhwajeong@gmail.com>
2021-11-04 15:54:12 -0400

    Add DivideNum Function

--- index.js
+++ index.js
@@ -3,101 +3,99 @@ let plusBtn = document.getElementById('plusBtn')
 let previous_operand = document.getElementById('previous_operand')
 let current_operand = document.getElementById('current_operand')
 let numberString = document.getElementsByClassName('numberBtn')
-let previousNum;
-let currentNum = "";
-let savedOperator;
-let number;
-let sum;
-let difference;
+let previousNum
+let currentNum = ''
+let savedOperator
+let number
+let sum
+let difference
+let divideResult
 
 // Combines string to be a single number of string
-function CombineStringNum(num)
-{
-    if (currentNum == 0)
-        currentNum = num;
-    else
-        currentNum += num;
-    
-    current_operand.innerText = currentNum;
+function CombineStringNum(num) {
+  if (currentNum == 0) currentNum = num
+  else currentNum += num
+
+  current_operand.innerText = currentNum
 }
 
-function SaveOperator(operator)
-{
-    savedOperator = operator;
+function SaveOperator(operator) {
+  savedOperator = operator
 }
 
 // Runs the function of an operator depending on which button is pressed
-function EqualOperator()
-{
-    switch(savedOperator)
-    {   
-        case "+":
-            AddNum();
-            break;
-        case "-":
-            SubtractNum();
-            break;
-    }
+function EqualOperator() {
+  switch (savedOperator) {
+    case '+':
+      AddNum()
+      break
+    case '-':
+      SubtractNum()
+      break
+    case '÷':
+      DivideNum()
+      break
+  }
 
-    previous_operand.innerText = current_operand.innerText +  " ";
+  previous_operand.innerText = current_operand.innerText + ' '
 }
 
 // Save previous numbers and operators used in a string to show in the output
-function SaveHistory()
-{
-    if (previous_operand.innerText == "")
-        previous_operand.innerText = current_operand.innerText +  " ";
-    else
-        previous_operand.innerText = current_operand.innerText +  " " + savedOperator;
+function SaveHistory() {
+  if (previous_operand.innerText == '')
+    previous_operand.innerText = current_operand.innerText + ' '
+  else
+    previous_operand.innerText = current_operand.innerText + ' ' + savedOperator
 }
 
 // Add 2 values together and save the sum to the previous operand string
-function AddNum()
-{
-    currentNum = parseInt(currentNum);
-    if (savedOperator == "+" || !savedOperator)
-    {
-        
-        if (previous_operand.innerText != "")
-        {
-            sum = parseInt(previous_operand.innerText) + currentNum;
-            current_operand.innerText = sum;
-        }
-        else
-            current_operand.innerText = currentNum;
+function AddNum() {
+  currentNum = parseInt(currentNum)
+  if (savedOperator == '+' || !savedOperator) {
+    if (previous_operand.innerText != '') {
+      sum = parseInt(previous_operand.innerText) + currentNum
+      current_operand.innerText = sum
+    } else current_operand.innerText = currentNum
 
-        SaveHistory();
-        
-    }
-    else
-    {
-        EqualOperator();
-        current_operand.innerText = sum;
-    }
-        
-    currentNum = "0";
+    SaveHistory()
+  } else {
+    EqualOperator()
+    current_operand.innerText = sum
+  }
 
+  currentNum = '0'
 }
 
-function SubtractNum()
-{
-    currentNum = parseInt(currentNum)
+function SubtractNum() {
+  currentNum = parseInt(currentNum)
 
-    if (savedOperator == "-" || !savedOperator)
-    {
-        if (previous_operand.innerText != "")
-        {
-            difference = parseInt(previous_operand.innerText) - currentNum
-            previous_operand.innerText = difference;
-        }
-        else
-            previous_operand.innerText = currentNum
+  if (savedOperator == '-' || !savedOperator) {
+    if (previous_operand.innerText != '') {
+      difference = parseInt(previous_operand.innerText) - currentNum
+      previous_operand.innerText = difference
+    } else previous_operand.innerText = currentNum
 
-        SaveHistory();
-    }
-    else
-        EqualOperator();
-    
-    current_operand.innerText = "0";
-    currentNum = "0";
+    SaveHistory()
+  } else EqualOperator()
+
+  current_operand.innerText = '0'
+  currentNum = '0'
+}
+
+function DivideNum() {
+  currentNum = parseInt(currentNum)
+
+  if (savedOperator == '÷' || !savedOperator) {
+    if (previous_operand.innerText != '') {
+      divideResult = parseInt(previous_operand.innerText) / currentNum
+      current_operand.innerText = divideResult
+    } else current_operand.innerText = currentNum
+
+    SaveHistory()
+  } else {
+    EqualOperator()
+    current_operand.innerText = '0'
+  }
+
+  currentNum = '0'
 }
